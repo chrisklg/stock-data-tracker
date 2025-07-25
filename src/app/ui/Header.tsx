abfb4eca03e0851c6187cfd61bf2ec68e19383e8
Christoph Klingenberg <christoph-klingenberg@web.de>
2025-07-25 18:02:00 +0200

    refactor Header

---
+++ src/app/ui/Header.tsx
@@ -0,0 +1,11 @@
+export default function Header() {
+  return (
+    <>
+      <header className="shadow-lg border-b-4 border-blue-400 sticky top-0">
+        <h2 className="text-2xl font-bold text-gray-900 dark:text-white font-mono">
+          Stock Data Tracker{" "}
+        </h2>
+      </header>
+    </>
+  );
+}
